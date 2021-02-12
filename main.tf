59d92c911fd7ec2af5c324d254c5d7f291c069d7
anishmoktan <anish.tmngo@gmail.com>
2021-02-11 20:22:40 -0500

    added bucket

--- main.tf
+++ main.tf
@@ -17,28 +17,38 @@ provider "aws" {
 
 # 1. Create VPC
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
 
 # 2. Create a private subnet
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "Private_Subnet"
-  }
-}
+#   tags = {
+#     Name = "Private_Subnet"
+#   }
+# }
 
 # 3. Create an s3 bucket
+
+# resource "aws_s3_bucket" "terraform_bucket" {
+#   bucket = "anishmoktanterraform"
+#   acl    = "private"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 
 # 4. Create an EC2 and install docker (use the user_data)
